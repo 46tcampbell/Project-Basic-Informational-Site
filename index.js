072173f232be22612bea3d7f43b51925b2258a56
46tcampbell <157092336+46tcampbell@users.noreply.github.com>
2026-06-16 11:19:00 -0500

    Create a basic informational site with node.js
    I tried to follow just the links in the (https://www.theodinproject.com/
    lessons/nodejs-getting-started) lesson, but was getting a little lost.
    I then watched the first 4 videos in this playlist (https://www.youtube.
    com/watch?v=bdHE2wHT-gQ&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=5)
    and this really helped click things in my brain.
    
    I decided to follow the YouTube tutorial closely and used the http and
    fs modules from Node to be able to create the logic to set up the
    server, listen for requests and respond correctly based on the incoming
    URL.
    
    Going to move forward with the lessons and will revisit this project if
    needed in the future.

---
+++ index.js
@@ -0,0 +1,41 @@
+import http from 'node:http';
+import fs from 'node:fs';
+
+// Create a local server to receive data from
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+  res.setHeader('Content-Type', 'text/html');
+
+  let path = './';
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    case '/contact-me':
+      path += 'contact-me.html';
+      res.statusCode = 200;
+      break;
+    default:
+      path += '404.html';
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
+});
+
+server.listen(8080, 'localhost', () => {
+  console.log('listening on port 8080');
+});
